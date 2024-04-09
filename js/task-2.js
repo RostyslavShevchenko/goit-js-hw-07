7b21dc805c2867e95d86a949021d3d60cb312daf
RostyslavShevchenko <shevchenkorostyslav005@gmail.com>
2024-04-09 21:52:29 +0300

    did 2 tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,29 +1,25 @@
-class Storage {
-  #items = [];
-
-  constructor(initialItems) {
-    this.#items = initialItems;
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
   }
+];
+const gallery = document.querySelector('.gallery');
 
-  getItems() {
-    return this.#items;
-  }
+images.forEach(image => {
+  const listItem = document.createElement('li');
+  const img = document.createElement('img');
 
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-
-  removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this.#items.splice(index, 1);
-    }
-  }
-}
-
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+  img.src = image.url;
+  img.alt = image.alt;
+  listItem.appendChild(img);
+  gallery.appendChild(listItem);
+});
