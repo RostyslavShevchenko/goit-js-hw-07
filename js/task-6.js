15bfc9a2d0850d834cdb5d840dfd134fb010808c
RostyslavShevchenko <shevchenkorostyslav005@gmail.com>
2024-04-14 22:38:20 +0300

    rework hw

--- js/task-6.js
+++ js/task-6.js
@@ -3,39 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const containerForBoxes = document.querySelector('#boxes');
+
+const input = document.querySelector('input[type="number"]');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
 
-function createBoxes(amount) {
-  containerForBoxes.innerHTML = '';
-
-  let boxSize = 30;
-  for (let i = 0; i < amount; i++) {
-    const newBox = document.createElement('div');
-    newBox.classList.add('box');
-    newBox.style.width = `${boxSize}px`;
-    newBox.style.height = `${boxSize}px`;
-    newBox.style.backgroundColor = getRandomHexColor();
-    containerForBoxes.appendChild(newBox);
-    boxSize += 10;
-  }
-}
-
-  function destroyBoxes () {
-  containerForBoxes.innerHTML = '';
-};
-
-  createBtn.addEventListener('click', function () {
-  const input = document.querySelector('input[type="number"]');
+createBtn.addEventListener('click', () => {
   const amount = parseInt(input.value);
 
-  if (amount < 1 || amount > 100) {
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
     alert('Please enter a number between 1 and 100.');
   }
+});
 
-  createBoxes(amount);
-  input.value = '';
-  });
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
 
-  destroyBtn.addEventListener('click', destroyBoxes);
+function createBoxes(amount) {
+  boxesContainer.innerHTML = '';
+
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const newBox = document.createElement('div');
+    newBox.style.width = `${size}px`;
+    newBox.style.height = `${size}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+    boxes.push(newBox);
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
